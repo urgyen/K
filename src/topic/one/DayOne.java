598dc8e8a44ef471b5af0ab77a7b4c79a5dded1e
Urgyen Shakya <urgyenshakya@gmail.com>
2020-07-21 08:39:18 +0545

    aa

---
+++ src/topic/one/DayOne.java
@@ -0,0 +1,126 @@
+package topic.one;
+
+public class DayOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+//		// VAriable
+//		// data type variable name?
+//		
+//		//Case sensitive;
+//		int age = 250; // 4 byte
+//		int Age= 250;
+//		
+//		String firstName= "Apple";
+//		
+//	//	String firstName= "Apple";
+//		
+//		//Lower
+//		//Upper 
+//		
+//		double age1= 250000000; //8 byte
+//		
+//		//bit and bytes;
+//		char letter= 'a';
+//		String name= "Urgyen";
+//		
+//		//Naming Conventions;  //Rules
+//		//	abc, ABC, 1abc
+//		
+//		
+//	//	Age;
+//	//	age;
+//		
+//		
+//		//Data types?
+//		// 1. PRimitive data type
+//		 
+//		 
+//		// 2. Non primitive data type
+//		 
+//		 
+//		 
+//		char   		a, b,c 		//Primitive
+//		String		Apple		//Non primitive
+//		
+//		//4 byte
+//		int			1	
+//		//8 byte
+//		double		1.1
+//		
+//		
+//		float		1.1
+//		
+//		array == colelction of numbers = {1,2,3,4,5}
+//		
+//		 
+//		// 1 bit ma define ??
+//		//bit and byte
+//		
+//		boolean isTrue = true;
+
+		// Operators?
+
+//		int num1 = 21;
+//		int num2 = 5;
+//		//Airthmetic; + -  * / %
+//		
+//		
+//		//remainder
+//		int result = num1 % num2;
+//		System.out.println(result);
+//
+//		int number = 19;
+//		
+////		odd%2 =1 
+////				even %2= 0
+////		
+//		if(number % 2 ==0) {
+//			System.out.println("Even");
+//		}
+//		else {
+//			System.out.println("ODD");
+//		}
+
+		// unary operator;
+		int a = 1;
+
+//		self study
+//		--a;
+//		++a;
+
+//		a++;
+//		a++;
+		a--; // a = a-1;
+		// a= a+1;
+
+		System.out.println(a);
+
+		// Logical Opertor
+
+		// And and OR??
+
+		// Logic Gates;
+
+//		t or f ==> f
+
+		// ternary Operator?
+
+		int num = 2;
+
+//		if(number % 2 ==0) {
+//			System.out.println("Even");
+//		}
+//		else {
+//			System.out.println("ODD");
+//		}
+
+		String result = ((num % 2) == 0) ? "Even" : "Odd";
+		System.out.println(result);
+
+		//
+
+	}
+
+}
