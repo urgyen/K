a1cd21c80521727806fd5649fcfbbac2167de53b
Urgyen Shakya <urgyenshakya@gmail.com>
2020-07-22 08:13:54 +0545

    Day 2

---
+++ src/topic/two/IFelseStatements.java
@@ -0,0 +1,95 @@
+package topic.two;
+
+public class IFelseStatements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	//	DEcision Control;
+		//	if condition
+		
+//		int age= 19;
+//		if(age>=18) {
+//			System.out.println("You can VOTE");
+//		}
+//		else {
+//			System.out.println("You cannot VOTE");
+//		}
+		
+		
+		//Sceanrio
+//		age 0-   10		Child
+//			31 - 50		Man
+		
+		
+		
+		
+		// 	if else
+		// 	if else if
+		
+//		int age=30;
+//		
+//		if(age<11) {
+//			System.out.println("Child");
+//		}
+//		
+//		else if(age>30  && age<51) {
+//			System.out.println("Man");
+//		}
+//		
+//		else {
+//			System.out.println("Its beyond Man and Child");
+//		}
+//		
+		
+//		if(age>0 && age<=10) {
+//			System.out.println("Child");
+//		}
+//		
+//		else if(age>=11 && age<=19) {
+//			System.out.println("Teen");
+//		}
+//		else if(age<31) {
+//			System.out.println("Adult");
+//		}
+//		else {
+//			System.out.println("Man");
+//		}
+		
+		
+		
+		//	Switch
+		
+		//condition k ho pailai thaha hunxa;
+		//definite cndition
+		
+		//1-sun, 2- monday..args. 7- Saturday
+		String  name= "jgjhgjh";
+		
+		char name1= 'a';
+		
+		switch(name) {
+		
+		case "Apple":
+			System.out.println("Sunday");
+			break;
+			
+		case "Banana":
+			System.out.println("Monday");
+			break;
+			
+		default:
+			System.out.println("Not Apple");
+			
+	
+			
+		}
+		//break bahyexi loop bahira aauxa
+		
+		
+		
+		
+		
+	}
+
+}
